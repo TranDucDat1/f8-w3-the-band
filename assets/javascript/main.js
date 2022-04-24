88bb174f24346532066abc8cf21868d623ad3815
Duc_Dat99 <datnho53@gmail.com>
2022-04-24 22:55:37 +0700

    Create Tuor Date

---
+++ assets/javascript/main.js
@@ -0,0 +1,33 @@
+// ***** DOM *****
+// 1. Element(h1, h2, p, a,...)
+// 2. Attributes(tittle, class, id, style, href,...)
+// 3. Text
+
+// 1.Get elements
+
+// var a = document.querySelector("#main #header #nav");
+
+// console.log(a);
+// console.log(a.querySelectorAll("li"));
+
+
+// 2.Attributes Node
+
+// var tourDate = document.getElementsByClassName("content-section")[1];
+// var sectionHeading = document.querySelectorAll("#content .content-section .section-heading")[1];
+// sectionHeading.setAttribute("style", "color:red");
+
+// 3.innerText VS textContent Property
+// innerText: chi in noi dung cua the(nhung thu duoc hien thi tren man hinh)
+// textContent: khong chi in nhung thu tren man hinh ma con in nhung thu da duoc an di(Vd: the co thuoc tinh display:none, the <style>, <script>)
+
+// var sectionHeading = document.querySelectorAll("#content .content-section .section-heading")[1];
+
+// 4.innnerHTML(Them mot element vao DOM) VS OuterHTML
+
+
+
+
+
+
+
