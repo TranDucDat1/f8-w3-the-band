c1b93eb0d242eb58f64954513ad131e4041c6698
Duc_Dat99 <datnho53@gmail.com>
2022-06-28 21:42:40 +0700

    Lau lam roi moi push len Git :D

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -45,17 +45,88 @@
 // Modal
 var modalClose = document.querySelector('.modal');
 
+modalClose.addEventListener('click', openOrCloseModal);
+
 function openOrCloseModal() {
-    modalClose.classList.add('modal-open');
+    modalClose.classList.toggle('modal-open');
 }
 
-var getCloseBtn = document.querySelector('.modal-close');
+var getCloseBtn = document.querySelectorAll('.open-close');
 
-getCloseBtn.addEventListener('click', openOrCloseModal);
+for(var i=0; i<getCloseBtn.length; i++){
+    getCloseBtn[i].addEventListener('click', openOrCloseModal);
+}
+
+// Xu ly khi nhan menu-icon thi se mo ra va dong lai mobile menu
+var mobileMenu = document.querySelector('.mobile-menu-btn');
+var header = document.querySelector('#header');
+var headerHeight = header.clientHeight;
+
+// An hien menu mobile
+mobileMenu.onclick = function() {
+    var isClose = header.clientHeight === headerHeight;
+
+    if(isClose) {
+        header.style.height = 'auto';
+    } else {
+        header.style.height = null;
+    }
+}
+
+// Tu dong dong khi chon the <li>
+var menuItems = document.querySelectorAll('#nav li a[href*="#"]')
+
+for(var i=0; i < menuItems.length; i++) {
+    var menuItem = menuItems[i];
+    
+    menuItem.onclick = function(e) {
+        var isParentMenu = this.nextElementSibling && this.nextElementSibling.classList.contains('subnav');
+        if(isParentMenu) {
+            e.preventDefault();
+        } else {
+            header.style.height = null;
+        }
+    }
+}
+
+// // JSON
+// var json = ["Javascript", "PHP"];
+
+// console.log(JSON.stringify(json));
+// // console.log(JSON.parse(json));
 
 
 
+// promise
 
+// setTimeout(function(){
+//     console.log(1);
+//     setTimeout(function(){
+//         console.log(2);
+//         setTimeout(function(){
+//             console.log(3);
+//         }, 1000);
+//     }, 1000);
+// }, 1000);
+
+// var promise = new Promise(
+//     // Executor
+//     function(resolve, reject) {
+//         // reject();
+//         // resolve();
+//     }
+// )
+
+// promise
+//     .then(function() {
+//         console.log("thanh cong!");
+//     })
+//     .catch(function() {
+//         console.log("that bai!");
+//     })
+//     .finally(function() {
+//         console.log("hoan thanh!");
+//     })
 
 
 
